1c18e84148844e7269a56acdd34773455e883bde
Pokey Rule <pokey.rule@globality.com>
2020-11-27 14:32:29 +0000

    Cleanup

--- main.ts
+++ main.ts
@@ -1,18 +1,11 @@
-import {
-  App,
-  Modal,
-  Notice,
-  Plugin,
-  PluginSettingTab,
-  Setting,
-  TFile
-} from "obsidian";
+import { Notice, Plugin } from "obsidian";
 
 const DEFAULT_PREAMBLE_PATH = "preamble.sty";
 
 export default class MyPlugin extends Plugin {
   async reload_preamble() {
     const file = this.app.vault.getAbstractFileByPath(DEFAULT_PREAMBLE_PATH);
+    console.log(`Loading preample from ${file}`);
     const content = await this.app.vault.read(file);
     MathJax.tex2chtml(content);
   }
